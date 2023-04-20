df35766bb5936771cdbabe42395442f49fe051be
Ali Hakimi <ali.hakimi.njf@gmail.com>
2023-04-20 14:23:41 +0430

    feat: base infrastructure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./pages/dynamic-form/dynamic-form.module').then(m => m.DynamicFormModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DynamicForm'`, () => {
+  it(`should have as title 'payeverCalender'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DynamicForm');
+    expect(app.title).toEqual('payeverCalender');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DynamicForm app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('payeverCalender app is running!');
   });
 });
